94b892f81a6846c16198db91fc22ae3acd0b837d
samzmn <sam.zmn99@gmail.com>
2026-01-03 04:44:11 +0330

    conditional dcgan trained with experience replay buffer

--- src/main.py
+++ src/main.py
@@ -23,7 +23,8 @@ def train_fashion_simple_gan(batch_size = 64, codings_dim = 30, g_lr = 1e-3, d_l
     result = trainer.train()
     print("Best:", result["best_epoch"], result["best_metric"], result["checkpoint_path"])
     utils.load_checkpoint(result["checkpoint_path"], generator, discriminator)
-    generate_new_images(generator, utils.normal_noise_sampler, codings_dim, device, save_path="./images/generated_fashion_mnist_with_simple_gan.png")
+    generate_new_images(generator, utils.normal_noise_sampler, codings_dim, device, 
+                        save_path="./images/generated_fashion_mnist_with_simple_gan.png")
 
 
 def train_fashion_deep_convolutional_gan(batch_size = 64, codings_dim = 100, g_lr = 1e-3, d_lr = 5e-4, seed = 42):
@@ -41,7 +42,8 @@ def train_fashion_deep_convolutional_gan(batch_size = 64, codings_dim = 100, g_l
     result = trainer.train()
     print("Best:", result["best_epoch"], result["best_metric"], result["checkpoint_path"])
     utils.load_checkpoint(result["checkpoint_path"], generator, discriminator)
-    generate_new_images(generator, utils.normal_noise_sampler, codings_dim, device, save_path="./images/generated_fashion_mnist_with_deep_conv_gan.png")
+    generate_new_images(generator, utils.normal_noise_sampler, codings_dim, device, 
+                        save_path="./images/generated_fashion_mnist_with_deep_conv_gan.png")
 
 
 def train_fashion_deep_conv_gan_with_replay_buffer(batch_size = 64, codings_dim = 100, g_lr = 1e-3, d_lr = 5e-4, seed = 42):
@@ -60,7 +62,8 @@ def train_fashion_deep_conv_gan_with_replay_buffer(batch_size = 64, codings_dim 
     result = trainer.train()
     print("Best:", result["best_epoch"], result["best_metric"], result["checkpoint_path"])
     utils.load_checkpoint(result["checkpoint_path"], generator, discriminator)
-    generate_new_images(generator, utils.normal_noise_sampler, codings_dim, device, save_path="./images/generated_fashion_mnist_with_deep_conv_gan.png")
+    generate_new_images(generator, utils.normal_noise_sampler, codings_dim, device, 
+                        save_path="./images/generated_fashion_mnist_with_deep_conv_gan_with_replay_buffer.png")
 
 
 def train_fashion_conditional_dcgan(batch_size = 64, codings_dim = 100, g_lr = 1e-3, d_lr = 5e-4, seed = 42):
@@ -79,14 +82,38 @@ def train_fashion_conditional_dcgan(batch_size = 64, codings_dim = 100, g_lr = 1
     result = trainer.train()
     print("Best:", result["best_epoch"], result["best_metric"], result["checkpoint_path"])
     utils.load_checkpoint(result["checkpoint_path"], generator, discriminator)
-    generate_new_images(generator, utils.normal_noise_sampler, codings_dim, device, num_classes=10, save_path="./images/generated_fashion_mnist_with_conditional_dcgan.png")
+    generate_new_images(generator, utils.normal_noise_sampler, codings_dim, device, num_classes=10, 
+                        save_path="./images/generated_fashion_mnist_with_conditional_dcgan.png")
+
+
+def train_fashion_conditional_dcgan_with_replay_buffer(batch_size = 64, codings_dim = 100, g_lr = 1e-3, d_lr = 5e-4, seed = 42):
+    device = utils.default_device()
+    data_loader = data.load_fashion_mnist(batch_size=batch_size, shuffle=True, conditional=True, seed=seed)
+    generator = models.ConditionalGenerator(codings_dim=codings_dim, num_classes=10, img_shape=[1, 28, 28]).to(device)
+    discriminator = models.ConditionalDiscriminator(num_classes=10, img_shape=[1, 28, 28]).to(device)
+    generator_opt = torch.optim.NAdam(generator.parameters(), lr=g_lr)
+    discriminator_opt = torch.optim.NAdam(discriminator.parameters(), lr=d_lr)
+    criterion = nn.BCEWithLogitsLoss()
+    early_stopping = train.EarlyStoppingConfig(monitor="g_loss", mode="min", patience=20)
+    train_config = train.TrainerConfig(out_dir=".", seed=42, epochs=100, device=device, use_tensorboard=False,
+                                       use_replay_buffer=True, replay_buffer_size=5000, replay_ratio=1.0)
+    trainer = train.GANTrainer(generator, discriminator, 
+                               model_name="conditional_dcgan_with_replay_buffer", dataset_name="fashion_mnist", num_classes=10,
+                               codings_dim=codings_dim, criterion=criterion, g_optimizer=generator_opt, d_optimizer=discriminator_opt,
+                               train_dataloader=data_loader, trainer_cfg=train_config, early_stopping=early_stopping)
+    result = trainer.train()
+    print("Best:", result["best_epoch"], result["best_metric"], result["checkpoint_path"])
+    utils.load_checkpoint(result["checkpoint_path"], generator, discriminator)
+    generate_new_images(generator, utils.normal_noise_sampler, codings_dim, device, num_classes=10, 
+                        save_path="./images/generated_fashion_mnist_with_conditional_dcgan_with_replay_buffer.png")
 
 
 def main():
     # train_fashion_simple_gan()
     # train_fashion_deep_convolutional_gan()
     # train_fashion_conditional_dcgan()
-    train_fashion_deep_conv_gan_with_replay_buffer()
+    # train_fashion_deep_conv_gan_with_replay_buffer()
+    train_fashion_conditional_dcgan_with_replay_buffer()
 
 if __name__ == "__main__":
     main()
